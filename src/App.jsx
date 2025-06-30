62335bc5d69eb97fd33084eda14976fe01e24093
Ville-Juhani Nivasalo <villejuhani.nivasalo@icloud.com>
2025-06-30 14:59:08 +0300

    login screen works 5_1

--- src/App.jsx
+++ src/App.jsx
@@ -64,13 +64,18 @@ const App = () => {
     </form>      
   )
 
+  if (user === null) {
+    return (
+      <div>
+        <h1>Please log in to application</h1>
+        <Notification message={errorMessage} />
+        {loginForm()}
+      </div>
+    )
+  }
+
   return (
     <div>
-      <Notification message={errorMessage} />
-      {!user && loginForm()}
-      {user && <div>
-      <p>{user.username} logged in</p>
-      </div>}
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
